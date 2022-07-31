e4400bb366f1a7b369633306368013be7955959e
iozberk <iozberks@gmail.com>
2022-07-31 16:09:20 +0300

    Schema Validation with Pydantic 001

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
@@ -7,6 +8,8 @@ app = FastAPI()
 class Post(BaseModel):
     title: str
     content: str
+    published: bool = True
+    rating : Optional [int] = None
 
 @app.get("/")
 def root():
@@ -17,6 +20,25 @@ def get_posts():
     return {"data": "Your Posts Endpoint"}
 
 @app.post("/createposts")
-def create_posts(new_post: Post):
-    print(new_post)
-    return {"data": "New Post Created"}
+def create_posts(post: Post):
+    print(post.title)
+    print(post.content)
+    print(post.published)
+    print(post.dict())
+    return {"data": post}
+
+
+""" POSTMAN 
+{
+    "detail": [
+        {
+            "loc": [
+                "body",
+                "content"
+            ],
+            "msg": "field required",
+            "type": "value_error.missing"
+        }
+    ]
+}
+"""
