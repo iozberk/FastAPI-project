80a23049c97815d09facdeebe093cc3489df1e08
iozberk <iozberks@gmail.com>
2022-08-03 00:02:28 +0300

    PostgreSQL connection 2

--- app/main.py
+++ app/main.py
@@ -1,8 +1,6 @@
-import re
-from textwrap import indent
+import time
 from typing import Optional
 from fastapi import FastAPI, Response, status, HTTPException
-from fastapi.params import Body
 import psycopg2
 from psycopg2.extras import RealDictCursor
 from pydantic import BaseModel
@@ -16,22 +14,24 @@ class Post(BaseModel):
     published: bool = True
     rating : Optional [int] = None
 
-try:
-    with open('dbpass.txt') as f:
-        lines = f.readlines()
-    dbpassword = lines[0]
-    f.close()
-    conn = psycopg2.connect(host="localhost", database="fastapi", user="postgres",
-    password=dbpassword, cursor_factory=RealDictCursor)
-    cursor = conn.cursor()
-    print("Database connection was successfully")
-    # cursor.execute("SELECT * FROM posts")
-    # posts = cursor.fetchall()
-    # conn.close()  
-except (Exception) as error:
-    print("Error while connecting to PostgreSQL", error)
-    print(error)
-
+while(True):
+    try:
+        with open('dbpass.txt') as f:
+            lines = f.readlines() 
+        dbpassword = lines[0]
+        f.close()
+        conn = psycopg2.connect(host="localhost", database="fastapi", user="postgres",
+        password=dbpassword, cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print("Database connection was successfully")
+        # cursor.execute("SELECT * FROM posts")
+        # posts = cursor.fetchall()
+        # conn.close()
+        break
+    except (Exception) as error:
+        print("Error while connecting to PostgreSQL", error)
+        print(error)
+        time.sleep(2)
 
 my_posts = [{"title" : "First Post Title", "content" : "First Post Content", "id": 1},
                 {"title" : "Second Post Title", "content" : "Second Post Content", "id": 2},
@@ -59,10 +59,6 @@ def get_posts():
 def create_posts(post: Post):
     post_dict = post.dict()
     post_dict['id'] = randrange(0,100000)
-    # print(post.title)
-    # print(post.content)
-    # print(post.published)
-    # print(post.dict())
     my_posts.append(post_dict)
     return {"data": post_dict}
  
@@ -72,10 +68,7 @@ def get_post(id: int, response: Response):
     post = find_post(id) 
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = f"Post id: {id} was NOT find")
-        # response.status_code = status.HTTP_404_NOT_FOUND
-        # return {"post_detail": f"Post id: {id} was not find"}
     print(post)
-    # return {"post_detail": f"Post id: {id} | and it returned by ID"}
     return {"post_detail": post}
 
 @app.delete("posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
@@ -98,8 +91,3 @@ def update_post(id : int, post: Post):
     post_dict['id'] = id
     my_posts[index] = post_dict
     return {"data": post_dict}
-
-# Branch test
-
-
- 
