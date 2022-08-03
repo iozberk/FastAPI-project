396c0583513f7bfc6026ce846b6c29487337d4c6
iozberk <iozberks@gmail.com>
2022-08-03 23:22:27 +0300

    Create Posts with code

--- app/main.py
+++ app/main.py
@@ -15,33 +15,33 @@ class Post(BaseModel):
     published: bool = True
     rating : Optional [int] = None
 
-try:
-    conn = psycopg2.connect(host = "ec2-176-34-215-248.eu-west-1.compute.amazonaws.com",database = "dc02oh10ouo2a2", user= "jjgpbdxckpkhrq", password="6c81246e0c47cfbda0202661616ff7cb659346a5c97b556cde0dbde9616ab79d", sslmode='require')
-    cursor = conn.cursor()
-    print("Database connection was successfully")
-except(Exception) as error:
-    print("Error while connecting to PostgreSQL", error)
-    print(error)
+# try:
+#     conn = psycopg2.connect(host = "ec2-176-34-215-248.eu-west-1.compute.amazonaws.com",database = "dc02oh10ouo2a2", user= "jjgpbdxckpkhrq", password="6c81246e0c47cfbda0202661616ff7cb659346a5c97b556cde0dbde9616ab79d", sslmode='require')
+#     cursor = conn.cursor()
+#     print("Database connection was successfully")
+# except(Exception) as error:
+#     print("Error while connecting to PostgreSQL", error)
+#     print(error)
 
 
-# while(True):
-#     try:
-#         with open('dbpass.txt') as f:
-#             lines = f.readlines() 
-#         dbpassword = lines[0]
-#         f.close()
-#         conn = psycopg2.connect(host="localhost", database="fastapi", user="postgres",
-#         password=dbpassword, cursor_factory=RealDictCursor)
-#         cursor = conn.cursor()
-#         print("Database connection was successfully")
-#         # cursor.execute("SELECT * FROM posts")
-#         # posts = cursor.fetchall()
-#         # conn.close()
-#         break
-#     except (Exception) as error:
-#         print("Error while connecting to PostgreSQL", error)
-#         print(error)
-#         time.sleep(2)
+while(True):
+    try:
+        with open('dbpass.txt') as f:
+            lines = f.readlines() 
+        dbpassword = lines[0]
+        f.close()
+        conn = psycopg2.connect(host="localhost", database="fastapi", user="postgres",
+        password=dbpassword, cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print("Database connection was successfully")
+        # cursor.execute("SELECT * FROM posts")
+        # posts = cursor.fetchall()
+        # conn.close()
+        break
+    except (Exception) as error:
+        print("Error while connecting to PostgreSQL", error)
+        print(error)
+        time.sleep(2)
 
 my_posts = [{"title" : "First Post Title", "content" : "First Post Content", "id": 1},
                 {"title" : "Second Post Title", "content" : "Second Post Content", "id": 2},
@@ -70,10 +70,14 @@ def get_posts():
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 def create_posts(post: Post):
-    post_dict = post.dict()
-    post_dict['id'] = randrange(0,100000)
-    my_posts.append(post_dict)
-    return {"data": post_dict}
+    # post_dict = post.dict()
+    # post_dict['id'] = randrange(0,100000)
+    # my_posts.append(post_dict)
+    new_post = cursor.execute(""" INSERT INTO post (title, content, published) VALUES (%s, %s, %s) RETURNING *  """, (post.title, post.content, post.published))
+    new_post = cursor.fetchone() 
+    conn.commit()
+
+    return {"data": new_post} 
  
 @app.get("/posts/{id}")
 def get_post(id: int, response: Response):
