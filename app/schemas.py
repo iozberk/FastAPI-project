ee529e162efc576e0fd1de0137559c12fb1fa425
iozberk <iozberks@gmail.com>
2022-08-07 15:20:06 +0300

    Verify user is Logged In

--- app/schemas.py
+++ app/schemas.py
@@ -1,3 +1,4 @@
+from typing import Optional
 from pydantic import BaseModel, EmailStr
 from datetime import datetime
 
@@ -45,4 +46,23 @@ class UserCreate(BaseModel):
 class UserLogin(BaseModel):
     email: EmailStr 
     password: str
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    id: Optional[str] = None
+    
+
+
+
+
+
+
+
+
+
+
+
 
