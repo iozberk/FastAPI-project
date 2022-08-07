85caf35fea0a0cf75cfd2ddc44db7e585813ae39
iozberk <iozberks@gmail.com>
2022-08-07 14:47:04 +0300

    Token has been received 2

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,8 +1,8 @@
-from os import access
-from fastapi import APIRouter, Depends, HTTPException, status, Response
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from app.database import get_db
-from .. import models, schemas, utils, oauth2
+from .. import models, utils, oauth2
 
 
 router = APIRouter(
@@ -10,14 +10,15 @@ router = APIRouter(
 )
 
 @router.post("/login")
-def login(user_credentials: schemas.UserLogin, db: Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+
+    user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid email or password")
     if not utils.verify(user_credentials.password, user.password):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Invalid email or password")
     access_token = oauth2.create_access_token(data={"user_id": user.id})
-    return {"access_token": access_token}
+    return {"access_token": access_token, "token_type": "bearer"} 
 
 
 
