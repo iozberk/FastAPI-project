f9fff1249c294e484da7bf64038590e8282ea210
intecon <iozberks@gmail.com>
2022-08-10 10:44:40 +0300

    some postman tests

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -6,11 +6,12 @@ from .. import models, utils, oauth2, schemas
 
 
 router = APIRouter(
-    tags=["Authentication - Login"]
+    tags=["Login"]
 )
 
 @router.post("/login", response_model=schemas.Token)
 def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+
 
     user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
     if not user:
@@ -19,9 +20,4 @@ def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session =
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid email or password")
     access_token = oauth2.create_access_token(data={"user_id": user.id})
     return {"access_token": access_token, "token_type": "bearer"} 
-
-
-
-
-
 
--- app/routers/post.py
+++ app/routers/post.py
@@ -18,7 +18,7 @@ def get_posts(db: Session = Depends(get_db), current_user : int = Depends(oauth2
     post = db.query(models.Post).all()
     return post
 
-@router.post("/", response_model=schemas.Post ,status_code=status.HTTP_201_CREATED)
+@router.post("/", response_model=schemas.Post, status_code=status.HTTP_201_CREATED)
 def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), current_user : int = Depends(oauth2.get_current_user)):
     new_post = models.Post(owner_id = current_user.id, **post.dict())
     db.add(new_post)
@@ -37,10 +37,15 @@ def get_post(id: int, db: Session = Depends(get_db), current_user : int = Depend
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id : int, db: Session = Depends(get_db), current_user : int = Depends(oauth2.get_current_user)):
-    post = db.query(models.Post).filter(models.Post.id == id)
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    post = post_query.first()
     if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"Post with id: {id} does NOT exist")
-    post.delete(synchronize_session=False)   
+    
+    if post.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not Auth to perform requested action")
+    
+    post_query.delete(synchronize_session=False)   
     db.commit() 
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
@@ -53,6 +58,10 @@ def update_post(id : int, updated_post: schemas.Post, db: Session = Depends(get_
 
     if post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= {"Post does NOT exist"})
+    
+    if post.owner_id != current_user.id:
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Not Auth to perform requested action")
+    
     post_query.update(updated_post.dict(),synchronize_session=False)   
     db.commit() 
     return post_query.first()
