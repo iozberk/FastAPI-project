26d7e1cba6fec841017d4aa339a17806fb9254e8
intecon <iozberks@gmail.com>
2022-08-11 11:12:26 +0300

    query parameters added

--- app/routers/post.py
+++ app/routers/post.py
@@ -13,9 +13,9 @@ router = APIRouter(
 
 
 @router.get("/", response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db), current_user : int = Depends(oauth2.get_current_user)):
+def get_posts(db: Session = Depends(get_db), current_user : int = Depends(oauth2.get_current_user), limit : int = 2):
     
-    post = db.query(models.Post).filter(models.Post.owner_id == current_user.id) .all()
+    post = db.query(models.Post).limit(limit).all()
     return post
 
 @router.post("/", response_model=schemas.Post, status_code=status.HTTP_201_CREATED)
--- app/schemas.py
+++ app/schemas.py
@@ -2,7 +2,6 @@ from pydantic import BaseModel, EmailStr
 from datetime import datetime
 from typing import Optional
 
-from pydantic.types import conint
 
 
 class PostBase(BaseModel):
