d0e95173ebaf47313e7583e26cd9a2738682f321
iozberk <iozberks@gmail.com>
2022-08-11 23:48:16 +0300

    Votes 2

--- app/models.py
+++ app/models.py
@@ -22,7 +22,7 @@ class User(Base):
     is_active = Column(Boolean, default=True)
     created_at = Column(TIMESTAMP(timezone=True), nullable=False, server_default=text('now()'))
 
-class Votes(Base):
-    __tablename__ = 'votes'
-    user_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'), primary_key=True)
-    post_id = Column(Integer, ForeignKey('posts.id', ondelete='CASCADE'), primary_key=True)
+class Vote(Base):
+    __tablename__ = "votes"
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), primary_key=True)
+    post_id = Column(Integer, ForeignKey("posts.id", ondelete="CASCADE"), primary_key=True)
