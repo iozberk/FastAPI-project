958b6cfa3daa8c0e88ca3176d02f4b0d0c755f8b
iozberk <iozberks@gmail.com>
2022-08-22 17:37:58 +0300

    conftest.py file created

---
+++ test/conftest.py
@@ -0,0 +1,50 @@
+from fastapi.testclient import TestClient
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from app.main import app
+from app.config import settings
+from app.database import get_db
+from app.database import Base
+
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+@pytest.fixture()
+def session():
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@pytest.fixture()
+def client(session):
+    def override_get_db():
+        try:
+            yield session
+        finally:
+            session.close()
+    app.dependency_overrides[get_db] = override_get_db
+    yield TestClient(app)
+
+@pytest.fixture
+def test_user(client):
+    user_data = {"email": "aaa@gmail.com", "password": "Password1!", "phone_number" : "555-555-5555"}
+    res = client.post("/users/", json=user_data)
+    assert res.status_code == 201
+    new_user = res.json()
+    new_user['password'] = user_data['password']
+    return new_user
+
+
+
+
+
+
+
+
+
--- test/test_users.py
+++ test/test_users.py
@@ -2,17 +2,8 @@ import pytest
 from jose import jwt
 from app.config import settings
 from app import schemas
-from .database import client, session
 
 
-@pytest.fixture
-def test_user(client):
-    user_data = {"email": "aaa@gmail.com", "password": "Password1!", "phone_number" : "555-555-5555"}
-    res = client.post("/users/", json=user_data)
-    assert res.status_code == 201
-    new_user = res.json()
-    new_user['password'] = user_data['password']
-    return new_user
 
     
 def test_create_user(client):
