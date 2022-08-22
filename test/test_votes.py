2e9ca1de768b0f32329bf3199c8a05c83c2cfc18
iozberk <iozberks@gmail.com>
2022-08-22 22:34:28 +0300

    test_vote_twice_post added

--- test/test_votes.py
+++ test/test_votes.py
@@ -1,6 +1,12 @@
+import pytest
+from app import models
 
-
-import json
+@pytest.fixture()
+def test_vote(test_post, session, test_user):
+    vote = models.Vote(post_id=test_post[3].id, user_id = test_user['id'])
+    session.add(vote)
+    session.commit()
+    return vote
 
 
 def test_vote_on_post(authorized_client, test_post):
@@ -8,3 +14,11 @@ def test_vote_on_post(authorized_client, test_post):
     assert res.status_code == 201
     print(res.json())
     assert res.json()['message'] == "successfully added vote"
+
+def test_vote_twice_post(authorized_client, test_post,test_vote):
+    res = authorized_client.post("/vote/", json={"post_id": test_post[3].id, "dir": 1})
+    assert res.status_code == 409
+    
+
+
+
