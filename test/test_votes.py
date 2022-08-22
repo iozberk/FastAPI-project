338e8ec96cf497b74869f7c53f37c7626899dbf0
iozberk <iozberks@gmail.com>
2022-08-22 22:38:31 +0300

    test_delete_vote_not_exist

--- test/test_votes.py
+++ test/test_votes.py
@@ -12,7 +12,7 @@ def test_vote(test_post, session, test_user):
 def test_vote_on_post(authorized_client, test_post):
     res = authorized_client.post("/vote/", json={"post_id": test_post[3].id, "dir": 1})
     assert res.status_code == 201
-    print(res.json())
+    # print(res.json())
     assert res.json()['message'] == "successfully added vote"
 
 def test_vote_twice_post(authorized_client, test_post,test_vote):
@@ -25,7 +25,9 @@ def test_vote_delete(authorized_client, test_post,test_vote):
     assert res.status_code == 201
     assert res.json()['message'] == "successfully deleted vote"
 
-
+def test_delete_vote_not_exist(authorized_client, test_post):
+    res = authorized_client.post("/vote/", json={"post_id": test_post[3].id, "dir": 0})
+    assert res.status_code == 404
 
 
 
