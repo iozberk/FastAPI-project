14ad403b629e18541c2267229419e59bb15000ba
iozberk <iozberks@gmail.com>
2022-08-22 19:03:19 +0300

    test_get_all_posts test post added

--- test/conftest.py
+++ test/conftest.py
@@ -1,3 +1,4 @@
+from turtle import title
 from fastapi.testclient import TestClient
 import pytest
 from sqlalchemy import create_engine
@@ -7,6 +8,7 @@ from app.config import settings
 from app.database import get_db
 from app.database import Base
 from app.oauth2 import create_access_token
+from app import models
 
 SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
@@ -51,6 +53,23 @@ def authorized_client(client, token):
     client.headers = {**client.headers,"Authorization": f"Bearer {token}"}
     return client
 
+@pytest.fixture
+def test_post(test_user, session):
+    post_data = [{"title": "Test Post 1", "content": "This is a test post 1", "owner_id": test_user['id']},
+                 {"title": "Test Post 2", "content": "This is a test post 2", "owner_id": test_user['id']},
+                 {"title": "Test Post 3", "content": "This is a test post 3", "owner_id": test_user['id']}]
+    def create_post_model(post):
+        return models.Post(**post)
 
+    post_map = map(create_post_model,post_data)
+    posts = list(post_map)
+    session.add_all(posts)
+    # session.add_all(models.Post(title= 'Test Post 1', content = 'This is a test post 1', owner_id =  test_user['id']),
+    #                 models.Post(title= 'Test Post 2', content = 'This is a test post 2', owner_id = test_user['id']),
+    #                 models.Post(title= 'Test Post 3', content = 'This is a test post 3', owner_id = test_user['id']),) 
+    session.commit()
+    posts = session.query(models.Post).all()                    
+
+    return posts
 
 
--- test/test_posts.py
+++ test/test_posts.py
@@ -1,6 +1,6 @@
 
 
-def test_get_all_posts(authorized_client):
+def test_get_all_posts(authorized_client, test_post):
     res = authorized_client.get("/posts/")
     print(res.json())
     assert res.status_code == 200
