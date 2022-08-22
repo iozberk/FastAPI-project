9ada6414df15b1f08c8b98901be9d413691bf220
iozberk <iozberks@gmail.com>
2022-08-22 17:42:25 +0300

    test_incorrect_login created

--- test/test_users.py
+++ test/test_users.py
@@ -21,3 +21,9 @@ def test_login_user(test_user, client):
     assert id == test_user['id']
     assert login_res.token_type == "bearer"
     assert res.status_code == 200
+
+
+def test_incorrect_login(test_user, client):
+    res = client.post("/login", data={"username": test_user['email'], "password": 'WRONGpassword'})
+    assert res.status_code == 403
+    assert res.json().get('detail') == "Invalid email or password"
